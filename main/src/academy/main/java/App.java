3a70fbf4bd3f6dc38b0741bd4ae14ae8ba64d2fb
v-almeida <almeida.vitor20000@gmail.com>
2026-09-02 19:38:59 -0300

    java1 done

--- main/src/academy/main/java/App.java
+++ main/src/academy/main/java/App.java
@@ -1,4 +1,6 @@
 package academy.main.java;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ main/src/academy/main/java/java1.java
@@ -0,0 +1,17 @@
+package academy.main.java;
+
+
+public class java1 {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        String name = "Jeff bezos";
+        String address = "boulivard street";
+        double payment = 300000.00;
+        String date = "09/02/28";
+
+        System.out.println("Me " +name+ " living at " +address+ " i've received the payment " +payment+ " on date " +date+ ".");
+
+
+    }
+}
